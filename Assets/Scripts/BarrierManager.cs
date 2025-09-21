02027af34ada959afd73ace3370e30be4c2da59e
Aiden Bain <aidenabain@gmail.com>
2025-09-21 16:01:52 -0600

    Updated Barrier, Enemy, and Player logic

--- Assets/Scripts/BarrierManager.cs
+++ Assets/Scripts/BarrierManager.cs
@@ -4,6 +4,10 @@ public class BarrierManager : MonoBehaviour, IDamageable
     [SerializeField] private int maxHealth;
     private int currentHealth;
 
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
     void IDamageable.TakeDamage(int damage)
     {
         currentHealth -= damage;
